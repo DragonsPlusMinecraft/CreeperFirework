c71192780b94dafff38e33f9ecf4f24b3b53a7f6
MarbleGateKeeper <marblegatekeeper@outlook.com>
2022-06-11 17:23:04 +0800

    Update to Minecraft 1.19

---
+++ remappedSrc/love/marblegate/creeperfirework/ClientCreeperFirework.java
@@ -0,0 +1,32 @@
+package love.marblegate.creeperfirework;
+
+import love.marblegate.creeperfirework.misc.FireworkManufacturer;
+import love.marblegate.creeperfirework.misc.NetworkUtil;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+
+public class ClientCreeperFirework implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        ClientPlayNetworking.registerGlobalReceiver(NetworkUtil.THE_VERY_I_DONT_WANT_TO_USE_PACKET, (client, handler, buf, responseSender) -> {
+            if (client.world != null) {
+                BlockPos pos = buf.readBlockPos();
+                boolean b = buf.readBoolean();
+                client.world.addFireworkParticle(pos.getX(), pos.getY() + 0.5F, pos.getZ(), 0, 0, 0, FireworkManufacturer.generate(b));
+                if (b) {
+                    client.world.addFireworkParticle(pos.getX(), pos.getY() + 2.5F, pos.getZ(), 0, 0, 0, FireworkManufacturer.generateRandomSpecial());
+                }
+                if (b) {
+                    client.world.playSound((PlayerEntity) null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_FIREWORK_ROCKET_LARGE_BLAST, SoundCategory.HOSTILE, 8.0F, 2.0F);
+                } else {
+                    client.world.playSound((PlayerEntity) null, pos.getX(), pos.getY(), pos.getZ(), SoundEvents.ENTITY_FIREWORK_ROCKET_BLAST, SoundCategory.HOSTILE, 8.0F, 2.0F);
+
+                }
+            }
+        });
+    }
+}
---
+++ remappedSrc/love/marblegate/creeperfirework/mixin/ExplosionMethodInvoker.java
@@ -0,0 +1,17 @@
+package love.marblegate.creeperfirework.mixin;
+
+import com.mojang.datafixers.util.Pair;
+import it.unimi.dsi.fastutil.objects.ObjectArrayList;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.explosion.Explosion;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(Explosion.class)
+public interface ExplosionMethodInvoker {
+    @Invoker("tryMergeStack")
+    public static void invokeTryMergeStack(ObjectArrayList<Pair<ItemStack, BlockPos>> stacks, ItemStack stack, BlockPos pos) {
+        throw new AssertionError();
+    }
+}
