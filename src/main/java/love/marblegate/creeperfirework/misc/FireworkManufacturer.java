b13d95b7d980d1212226c6b9f5d4621233c32483
MarbleGateKeeper <marblegatekeeper@outlook.com>
2022-07-08 23:58:02 +0800

    1.2.2
    [fix] Chinese Config Ambiguous Items are fixed
    [fix] English Config Typo Item is fixed
    [refact] Code Cleanup

--- src/main/java/love/marblegate/creeperfirework/misc/FireworkManufacturer.java
+++ src/main/java/love/marblegate/creeperfirework/misc/FireworkManufacturer.java
@@ -4,28 +4,14 @@ import com.google.common.collect.Lists;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.util.DyeColor;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 import java.util.Random;
 
 public class FireworkManufacturer {
     public static NbtCompound generate(boolean powered) {
-        NbtCompound fireworkInfoNbt = new NbtCompound();
-
-        // Generate Color From DyeColor
-        // Maximum is 8 dyes for a firework star
-        List<Integer> list = Lists.newArrayList();
-        Random rand = new Random();
-        int dyeCount = rand.nextInt(8) + 1;
-        for (int i = 0; i < dyeCount; i++)
-            list.add(DyeColor.values()[rand.nextInt(DyeColor.values().length)].getFireworkColor());
-
-        // Transform color array into required structure
-        int[] colours = new int[list.size()];
-        for (int i = 0; i < colours.length; i++)
-            colours[i] = list.get(i);
-
-        fireworkInfoNbt.putIntArray("Colors", colours);
+        NbtCompound fireworkInfoNbt = getFireworkInfoNbt();
         fireworkInfoNbt.putBoolean("Flicker", powered);
         fireworkInfoNbt.putByte("Type", (byte) (powered ? 1 : 0));
         fireworkInfoNbt.putBoolean("Trail", powered);
@@ -38,6 +24,18 @@ public class FireworkManufacturer {
     }
 
     public static NbtCompound generateRandomSpecial() {
+        NbtCompound fireworkInfoNbt = getFireworkInfoNbt();
+        fireworkInfoNbt.putByte("Type", (byte) (new Random().nextInt(3) + 2));
+        NbtList mimicFireworkItemNbtStructureContainer = new NbtList();
+        mimicFireworkItemNbtStructureContainer.add(fireworkInfoNbt);
+
+        NbtCompound ret = new NbtCompound();
+        ret.put("Explosions", mimicFireworkItemNbtStructureContainer);
+        return ret;
+    }
+
+    @NotNull
+    private static NbtCompound getFireworkInfoNbt() {
         NbtCompound fireworkInfoNbt = new NbtCompound();
 
         // Generate Color From DyeColor
@@ -54,12 +52,6 @@ public class FireworkManufacturer {
             colours[i] = list.get(i);
 
         fireworkInfoNbt.putIntArray("Colors", colours);
-        fireworkInfoNbt.putByte("Type", (byte) (rand.nextInt(3) + 2));
-        NbtList mimicFireworkItemNbtStructureContainer = new NbtList();
-        mimicFireworkItemNbtStructureContainer.add(fireworkInfoNbt);
-
-        NbtCompound ret = new NbtCompound();
-        ret.put("Explosions", mimicFireworkItemNbtStructureContainer);
-        return ret;
+        return fireworkInfoNbt;
     }
 }
